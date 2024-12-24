44e24938ad63ea5ac3f42f913dfeb700eda5fb27
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-24 15:40:19 +0300

    Fixing bugs

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,7 +2,7 @@
 URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -27,4 +27,3 @@ urlpatterns = [
     path('api-auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(main_api_urlpatterns)),
 ]
-
