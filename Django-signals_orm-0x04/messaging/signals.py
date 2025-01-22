802d8344b6c846ceddde6d4a74299350c8b498f8
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-22 17:45:16 +0300

    deleting user

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 
@@ -20,3 +21,16 @@ def log_message_edit(sender, instance, **kwargs):
             )
             # Mark the message as edited
             instance.edited = True
+
+@receiver(post_delete, sender=User)
+def clean_up_user_data(sender, instance, **kwargs):
+    # Delete all messages where the user is the sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete all notifications for the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete all message histories for the user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+def delete_user(request):
+    if request.method == "POST":
+        user = request.user
+        user.delete()
+        messages.success(request, "Your account and related data have been deleted.")
+        return redirect("home")
