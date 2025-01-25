6dde508649435fae9b866ee40fc7bd509141d883
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-01-26 00:51:14 +0300

    updated Dockerfile

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,23 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_nested.routers import  NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet, UserCreateView
 
+# Create a regular router for the conversations and messages
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='conversation-messages')  # Regular route for messages
 
-conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
-conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
-
+# Create a regular router for the user-related endpoints
 auth_router = routers.DefaultRouter()
 auth_router.register(r'users', UserCreateView, basename='users')
 
+# Define the main API URL patterns
 main_api_urlpatterns = [
     path('', include(router.urls)),
-    path('', include(conversations_router.urls)),
 ]
 
+# Define the auth API URL patterns
 auth_api_urlpatterns = [
-    path('', include(auth_router.urls)), 
+    path('', include(auth_router.urls)),
 ]
-
